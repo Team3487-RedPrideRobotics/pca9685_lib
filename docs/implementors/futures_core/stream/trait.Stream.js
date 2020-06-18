46774f4a69d5b502e4dd7f2bd8bcc446d2599bc7
Jake Armstrong <Lainkai@users.noreply.github.com>
2020-06-18 12:01:49 -0400

    Reloaded Documentation

---
+++ docs/implementors/futures_core/stream/trait.Stream.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["futures_core"] = [];
+implementors["piper"] = [{"text":"impl&lt;T&gt; <a class=\"trait\" href=\"futures_core/stream/trait.Stream.html\" title=\"trait futures_core::stream::Stream\">Stream</a> for <a class=\"struct\" href=\"piper/struct.Receiver.html\" title=\"struct piper::Receiver\">Receiver</a>&lt;T&gt;","synthetic":false,"types":["piper::chan::Receiver"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ docs/implementors/futures_sink/trait.Sink.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["futures_sink"] = [];
+implementors["piper"] = [{"text":"impl&lt;T&gt; <a class=\"trait\" href=\"futures_sink/trait.Sink.html\" title=\"trait futures_sink::Sink\">Sink</a>&lt;T&gt; for <a class=\"struct\" href=\"piper/struct.Sender.html\" title=\"struct piper::Sender\">Sender</a>&lt;T&gt;","synthetic":false,"types":["piper::chan::Sender"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
