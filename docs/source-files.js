7550696ffe3d4918e5005ec68a7c4fd552190ec9
Jake Armstrong <Lainkai@users.noreply.github.com>
2020-06-18 11:52:25 -0400

    Added Docs

---
+++ docs/source-files.js
@@ -0,0 +1,7 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["pin_project_internal"] = {"name":"","dirs":[{"name":"pin_project","files":["attribute.rs","derive.rs","mod.rs"]}],"files":["lib.rs","pinned_drop.rs","project.rs","utils.rs"]};
+sourcesIndex["proc_macro2"] = {"name":"","files":["detection.rs","fallback.rs","lib.rs","parse.rs","wrapper.rs"]};
+sourcesIndex["quote"] = {"name":"","files":["ext.rs","format.rs","ident_fragment.rs","lib.rs","runtime.rs","spanned.rs","to_tokens.rs"]};
+sourcesIndex["syn"] = {"name":"","dirs":[{"name":"gen","files":["gen_helper.rs","visit.rs","visit_mut.rs"]}],"files":["attr.rs","bigint.rs","buffer.rs","custom_keyword.rs","custom_punctuation.rs","data.rs","derive.rs","discouraged.rs","error.rs","export.rs","expr.rs","ext.rs","file.rs","generics.rs","group.rs","ident.rs","item.rs","lib.rs","lifetime.rs","lit.rs","lookahead.rs","mac.rs","macros.rs","op.rs","parse.rs","parse_macro_input.rs","parse_quote.rs","pat.rs","path.rs","print.rs","punctuated.rs","sealed.rs","span.rs","spanned.rs","stmt.rs","thread.rs","token.rs","ty.rs","verbatim.rs"]};
+sourcesIndex["unicode_xid"] = {"name":"","files":["lib.rs","tables.rs"]};
+createSourceSidebar();
